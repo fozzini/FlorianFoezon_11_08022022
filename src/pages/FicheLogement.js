9fc9e4c412208786317e9ed6b47e5b089de6bf99
florian <ffoezon@gmail.com>
2022-02-20 19:01:52 +0100

    css ok react ok

--- src/pages/FicheLogement.js
+++ src/pages/FicheLogement.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import Accordion from '../composants/Accordeon';
 import Carousel from '../composants/Carousel';
 import Footer from "../composants/Footer";
 import Header from "../composants/Header";
@@ -17,37 +18,36 @@ const FicheLogement = ({data}) => {
         .map((element, index) => (
         <>
           <Carousel slideData={element} key={index}/>
-          <div>
-            <div>
+          <div className='topSection'>
+            <div className='leftTopSection'>
               <h2>{element.title}</h2>
               <h3>{element.location}</h3>
             </div>
-            <div>
-              <h3>{element.host.name}</h3>
-              <div>
+            <div className='rightTopSection'>
+              <div className='nameContainer'>{element.host.name
+              .split(" ")
+              .map((elmt) => <h3>{elmt}</h3>)}
+              </div> 
+              <div className='pictureContainer'>
                 <img src={element.host.picture} alt="propriétaire"/>
               </div>
             </div>
-            <div>
-              <div>
-                {element.tags.map((tag, index) => <h3 key={index}>{tag}</h3>)}
-              </div>
-              <div>
-                <StarRating rating={element.rating} key={element.id}/>
-              </div>
-              <div>
-                {/* {element
-                 .map(({ element.description.value(), element.description.key() }) => (<Accordion key={content} heading={heading} content={content} />))} */}
-              </div>
-            </div>
           </div>
-          
-          
+          <div className='middleSection'>
+            <div className='leftMiddleSection'>
+              {element.tags.map((tag, index) => <h3 key={index}>{tag}</h3>)}
+            </div>
+            <div className='rightMiddleSection'>
+              <StarRating rating={element.rating} key={element.id}/>
+            </div>
+          </div>  
+          <div className='bottomSection'>
+            <ul className="accordion left"><Accordion heading="Description" content={element.description}/></ul>
+            <ul className="accordion right"><Accordion heading="Equipements" content={element.equipments.map((equipment) => <li>{equipment}</li>)}/></ul>
+          </div>
         </>
         ))
-        
       }
-      
       <Footer/>
     </div>
   )
